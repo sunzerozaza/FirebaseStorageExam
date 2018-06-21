50e3ae38fdd24e0594be90635b3f1670ce1bc540
SunZeRo <sunzamahachai@gmail.com>
2018-06-21 14:18:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sunzero/firebasestorageexam/Users.java
@@ -0,0 +1,44 @@
+package com.example.sunzero.firebasestorageexam;
+
+/**
+ * Created by SunZeRo on 21/6/2561.
+ */
+
+public class Users {
+    public String name, image, status;
+
+    public Users(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Users(String name, String image, String status) {
+        this.name = name;
+        this.image = image;
+        this.status = status;
+    }
+}
+
